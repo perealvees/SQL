29d958caf800c91ad6b5e56d267d5e8382332dde
perealvees <perealvees@gmail.com>
2023-09-14 14:10:22 -0300

    Funções de Agregações

---
+++ Hashtag Programação/Aula Hashtag_Aula 3.sql
@@ -0,0 +1,7 @@
+-- mostre apenas produtos com valores maiores que 1800 da tabela Produtos
+SELECT *
+FROM produtos
+WHERE Preco_Unit > '1800';
+
+-- mostre os pedidos feito no dia 03/01/2019
+-- mostre clientes solteiros e sexo masculino
