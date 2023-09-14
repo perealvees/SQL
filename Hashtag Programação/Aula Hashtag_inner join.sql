ae2825d3d8cb3aaff57d4330bb93bc64d8bdd7f3
perealvees <perealvees@gmail.com>
2023-09-14 20:58:38 -0300

    inner join

---
+++ Hashtag Programação/Aula Hashtag_inner join.sql
@@ -0,0 +1,6 @@
+select * from pedidos;
+select * from lojas;
+-- 1º PASSO: identificar quem é a tabela fato e tabela dimensão
+-- tabela fato vai ser pedidos, a dimensão vai ser a tabela loja
+-- 2º PASSO: quem vai ser a chave primária e chave estrangeira
+-- chave primaria
